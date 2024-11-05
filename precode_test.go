8271d766b1c8765ec1d24e572accdf223ace7ae9
Ilya Alekseev <phone192@yandex.ru>
2024-11-05 15:37:11 +0300

    111

---
+++ precode_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Запрос сформирован корректно, сервис возвращает
+// - код ответа 200 и
+// - тело ответа не пустое
+func TestMainHandlerForCorrectRequest(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code should be 200")
+	assert.NotEmpty(t, responseRecorder.Body.String(), "Response body should not be empty")
+}
+
+// Город, который передаётся в параметре city, не поддерживается.
+// Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
+func TestMainHandlerForCorrectCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=tula", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Expected response code for missing city should be 400")
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String(), "Expected response body for missing city is 'wrong city value'")
+}
+
+// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе.
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	assert.Len(t, list, totalCount)
+
+}
